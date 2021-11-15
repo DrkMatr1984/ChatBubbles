11d25ecd550975a919f069b0592859aea950f231
Nicolas Shreiner <DrkMatr1984@aol.com>
2021-11-15 07:56:59 -0500

    Update handleThree.java

--- src/main/java/me/TheTealViper/chatbubbles/handleThree.java
+++ src/main/java/me/TheTealViper/chatbubbles/handleThree.java
@@ -16,57 +16,61 @@ public class handleThree {
 		boolean sendOriginal = cb.getConfig().getBoolean("ChatBubble_Send_Original_Message");
 		boolean requirePerm = cb.getConfig().getBoolean("ConfigOne_Require_Permissions");
 		String usePerm = cb.getConfig().getString("ConfigOne_Use_Permission");
-		if(requirePerm && !p.hasPermission(usePerm))
-			return;
-		if(cb.existingHolograms.containsKey(p.getUniqueId())) {
-			for(Hologram h : cb.existingHolograms.get(p.getUniqueId())) {
-				if(!h.isDeleted())
-					h.delete();
-			}
-		}
-		MPlayer mPlayer = MPlayer.get(p);
-		String faction = mPlayer.getFactionName();
-		final Hologram hologram = HologramsAPI.createHologram(cb, p.getLocation().add(0.0, cb.bubbleOffset, 0.0));
-		List<Hologram> hList = new ArrayList<Hologram>();
-		hList.add(hologram);
-		cb.existingHolograms.put(p.getUniqueId(), hList);
-		hologram.getVisibilityManager().setVisibleByDefault(false);
-		for(Player oP : Bukkit.getOnlinePlayers()){
-			if(((cb.seeOwnBubble) || (!cb.seeOwnBubble && oP.getName() != p.getName())) 
-					&& (oP.getWorld().getName().equals(p.getWorld().getName()) 
-					&& oP.getLocation().distance(p.getLocation()) <= cb.distance) 
-					&& (MPlayer.get(oP).getFactionName().equals(faction))
-					&& oP.canSee(p))
-				hologram.getVisibilityManager().showTo(oP);
-		}
-		int lines = cb.formatHologramLines(p, hologram, message);
-		if(sendOriginal)
-			p.chat(message);
-
 		new BukkitRunnable() {
-			int ticksRun = 0;
 			@Override
 			public void run() {
-				ticksRun++;
-				if(!hologram.isDeleted())
-					hologram.teleport(p.getLocation().add(0.0, cb.bubbleOffset + .25 * lines, 0.0));
-				if (ticksRun > cb.life) {
-					hologram.delete();
-					cancel();
+				if(requirePerm && !p.hasPermission(usePerm))
+					return;
+				if(cb.existingHolograms.containsKey(p.getUniqueId())) {
+					for(Hologram h : cb.existingHolograms.get(p.getUniqueId())) {
+						if(!h.isDeleted())
+							h.delete();
+					}
 				}
-		}}.runTaskTimer(cb, 1L, 1L);
-		
-		if(cb.getConfig().getBoolean("ChatBubble_Play_Sound")) {
-			String sound = cb.getConfig().getString("ChatBubble_Sound_Name").toLowerCase();
-			float volume = (float) cb.getConfig().getDouble("ChatBubble_Sound_Volume");
-			if(!sound.equals("")) {
-				try {
-					p.getWorld().playSound(p.getLocation(), sound, volume, 1.0f);
-				}catch(Exception e) {
-					cb.getServer().getConsoleSender().sendMessage("Something is wrong in your ChatBubble config.yml sound settings!");
-					cb.getServer().getConsoleSender().sendMessage("Please ensure that 'ChatBubble_Sound_Name' works in a '/playsound' command test.");
+				MPlayer mPlayer = MPlayer.get(p);
+				String faction = mPlayer.getFactionName();
+				final Hologram hologram = HologramsAPI.createHologram(cb, p.getLocation().add(0.0, cb.bubbleOffset, 0.0));
+				List<Hologram> hList = new ArrayList<Hologram>();
+				hList.add(hologram);
+				cb.existingHolograms.put(p.getUniqueId(), hList);
+				hologram.getVisibilityManager().setVisibleByDefault(false);
+				for(Player oP : Bukkit.getOnlinePlayers()){
+					if(((cb.seeOwnBubble) || (!cb.seeOwnBubble && oP.getName() != p.getName())) 
+							&& (oP.getWorld().getName().equals(p.getWorld().getName()) 
+							&& oP.getLocation().distance(p.getLocation()) <= cb.distance) 
+							&& (MPlayer.get(oP).getFactionName().equals(faction))
+							&& oP.canSee(p))
+						hologram.getVisibilityManager().showTo(oP);
 				}
-			}
-		}
+				int lines = cb.formatHologramLines(p, hologram, message);
+				if(sendOriginal)
+					p.chat(message);
+
+				new BukkitRunnable() {
+					int ticksRun = 0;
+					@Override
+					public void run() {
+						ticksRun++;
+						if(!hologram.isDeleted())
+							hologram.teleport(p.getLocation().add(0.0, cb.bubbleOffset + .25 * lines, 0.0));
+						if (ticksRun > cb.life) {
+							hologram.delete();
+							cancel();
+						}
+				}}.runTaskTimer(cb, 1L, 1L);
+				
+				if(cb.getConfig().getBoolean("ChatBubble_Play_Sound")) {
+					String sound = cb.getConfig().getString("ChatBubble_Sound_Name").toLowerCase();
+					float volume = (float) cb.getConfig().getDouble("ChatBubble_Sound_Volume");
+					if(!sound.equals("")) {
+						try {
+							p.getWorld().playSound(p.getLocation(), sound, volume, 1.0f);
+						}catch(Exception e) {
+							cb.getServer().getConsoleSender().sendMessage("Something is wrong in your ChatBubble config.yml sound settings!");
+							cb.getServer().getConsoleSender().sendMessage("Please ensure that 'ChatBubble_Sound_Name' works in a '/playsound' command test.");
+						}
+					}
+				}
+	    }}.runTask(cb);
 	}
 }
